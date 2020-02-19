52af2eca0b72b0081f06a9c5366e95ebb106baa0
Adam Quinlan <aquinlan@tilsontech.com>
2020-02-19 05:04:49 -0500

    hover animations and cleanup of top nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,21 +3,21 @@
     <v-app-bar fixed app class="white">
       <img src="https://via.placeholder.com/200x50" class="header-logo" alt="NearPeer Logo">
       <v-spacer />
-      <div class="navlink deep-purple--text text--darken-3 active">
-        Higher Enrollment
-      </div>
-      <div class="navlink deep-purple--text text--darken-3">
-        Student Success
-      </div>
-      <div class="navlink deep-purple--text text--darken-3">
-        What is NearPeer?
-      </div>
-      <div class="navlink deep-purple--text text--darken-3">
-        About
-      </div>
-      <div class="contact-link pink accent-2 white--text">
-        Contact Us
-      </div>
+      <nuxt-link to="/#" class="navlink deep-purple--text text--darken-3">
+          Higher Enrollment
+      </nuxt-link>
+      <nuxt-link to="/#" class="navlink deep-purple--text text--darken-3">
+          Student Success
+      </nuxt-link>
+      <nuxt-link to="/#" class="navlink deep-purple--text text--darken-3">
+          What is NearPeer?
+      </nuxt-link>
+      <nuxt-link to="/#" class="navlink deep-purple--text text--darken-3">
+          About
+      </nuxt-link>
+      <nuxt-link to="/#" class="contact-link pink accent-2 white--text">
+          Contact Us
+      </nuxt-link>
     </v-app-bar>
     <v-content>
       <v-container>
@@ -71,16 +71,18 @@ export default {
 
 <style lang="scss" scoped>
 .navlink{
+    text-decoration: none;
     margin: 0 16px;
     height: calc(100% + 8px);
     display: flex;
     align-items: center;
     justify-content: center;
-    cursor: pointer;
     font-size: 13px;
     font-weight: 600;
     letter-spacing: 0.03em;
-  &.active{
+    border-top: 5px solid transparent;
+    padding-bottom: 5px;
+  &.active,&:hover{
     border-top: 5px solid #FF4081;
   }
 }
@@ -89,6 +91,7 @@ export default {
   font-size: 13px;
   border-radius: 20px;
   margin-left: 15px;
+  text-decoration: none;
 }
 
 .footer-area{
