716fc6d060c9b55fb0d6de79ffcb8327379634b6
nickonometry <tylerforge@tylertech.com>
2020-02-20 09:05:44 -0500

    added hero image component

---
+++ components/HeroImage.vue
@@ -0,0 +1,25 @@
+
+
+<template>
+  <div class="hero-container">
+    <img src="/images/homepage-hero/hero1.jpg" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.hero-container {
+  height: 600px;
+  background: #4611a9;
+  position: relative;
+
+  img {
+    mix-blend-mode: screen;
+    opacity: 0.3;
+    width: 100%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-      <v-row align="center" justify="center">
-        <v-col class="text-center" cols="12">
-          <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
-          <h4 class="subheading">Build your application today!</h4>
-        </v-col>
-      </v-row>
-    </v-parallax>
+    <HeroImage />
 
     <section class="enrollment-rate">
       <div class="enrollment-rate__grid">
@@ -25,7 +18,13 @@
 </template>
 
 <script>
-export default {};
+import HeroImage from "../components/HeroImage";
+
+export default {
+  components: {
+    HeroImage
+  }
+};
 </script>
 
 <style lang="scss" scoped>
