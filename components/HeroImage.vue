a0888f28e2ea9280ee188be269fb3e02666e1d5e
nickonometry <tylerforge@tylertech.com>
2020-02-20 09:25:59 -0500

    added gradient

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -2,7 +2,18 @@
 
 <template>
   <div class="hero-container">
-    <v-img src="/images/homepage-hero/hero1.jpg" aspect-ratio="3.2"></v-img>
+    <v-img
+      src="/images/homepage-hero/hero1.jpg"
+      lazy-src="/images/homepage-hero/hero1.jpg"
+      aspect-ratio="3.2"
+    >
+      <div class="fill-height bottom-gradient"></div>
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
   </div>
 </template>
 
@@ -14,13 +25,19 @@ export default {};
 .hero-container {
   height: 100%;
   max-height: 600px;
-  background: #4611a9;
-  position: relative;
 
   img {
-    mix-blend-mode: screen;
-    opacity: 0.3;
-    width: 100%;
+    // mix-blend-mode: screen;
+    // opacity: 0.3;
+    // width: 100%;
   }
+}
+
+.bottom-gradient {
+  background-image: linear-gradient(
+    to right,
+    rgba(70, 17, 169, 0.75) 100%,
+    transparent 72px
+  );
 }
 </style>
