cde4d36ec82a6c79f886f8aef30b755624484645
nickonometry <nickonometry@gmail.com>
2020-02-20 21:32:06 -0500

    kinda mobile friendly header

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -7,17 +7,15 @@
     <div class="slides">
       <div class="slide-1"></div>
       <div class="hero">
-        <hgroup>
-          <h2>
-            Harness the power of
-            <span class="underline">peer</span>
-            <span class="underline">engagement</span>
-          </h2>
-          <h3>
-            Leverage peer networks for more committed,
-            enrolled successful students
-          </h3>
-        </hgroup>
+        <h2>
+          Harness the power of
+          <span class="underline">peer</span>
+          <span class="underline">engagement</span>
+        </h2>
+        <p>
+          Leverage peer networks for more committed,
+          enrolled successful students
+        </p>
       </div>
     </div>
   </div>
@@ -31,29 +29,46 @@ export default {};
 .hero-container {
   position: relative;
   height: 600px;
-}
 
-.hero {
-  position: absolute;
-  top: 10%;
-  left: 5%;
-  z-index: 3;
-  color: #fff;
-  text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.75);
-  max-width: 550px;
-
-  h2 {
-    font-size: 4em;
-    font-weight: bold;
-    margin-bottom: 48px;
-    padding: 0;
-    line-height: 74px;
+  .slides {
+    .slide-1 {
+      height: 600px;
+      background-size: cover;
+      background-position: center center;
+      background-repeat: no-repeat;
+      background-image: url(/images/homepage-hero/hero1.jpg);
+    }
   }
 
-  .underline {
-    border-bottom: 8px solid #ff4081;
-    display: inline-block;
-    line-height: 0.85;
+  .hero {
+    position: absolute;
+    top: 10%;
+    left: 5%;
+    z-index: 3;
+    color: #fff;
+    text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.75);
+    max-width: 550px;
+
+    h2 {
+      font-size: 4em;
+      font-weight: bold;
+      margin-bottom: 48px;
+      padding: 0;
+      line-height: 74px;
+    }
+
+    .underline {
+      border-bottom: 8px solid #ff4081;
+      display: inline-block;
+      line-height: 0.85;
+    }
+
+    p {
+      line-height: 24px;
+      font-size: 24px;
+      margin-bottom: 48px;
+      max-width: 500px;
+    }
   }
 }
 
@@ -67,19 +82,6 @@ export default {};
     rgba(70, 17, 169, 0.8),
     rgba(70, 17, 169, 0.95)
   );
-  // opacity: 0.85;
-  // mix-blend-mode: multiply;
-}
-
-.hero-container .slides .slide-1 {
-  height: 600px;
-  background-size: cover;
-  background-position: center center;
-  background-repeat: no-repeat;
-}
-
-.hero-container .slides .slide-1 {
-  background-image: url(/images/homepage-hero/hero1.jpg);
 }
 
 /********************************/
@@ -91,9 +93,41 @@ export default {};
   }
 }
 @media screen and (max-width: 640px) {
-  .hero {
-    h2 {
-      font-size: 4em;
+  .hero-container {
+    position: relative;
+    height: 100vh;
+
+    .slides {
+      .slide-1 {
+        height: 100vh;
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-image: url(/images/homepage-hero/hero1.jpg);
+      }
+    }
+
+    .hero {
+      margin: auto;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      height: 100vh;
+      padding: 3%;
+
+      h2 {
+        font-size: 48px;
+        font-weight: bold;
+        margin-bottom: 56px;
+        padding: 0;
+        line-height: 48px;
+      }
     }
   }
 }
