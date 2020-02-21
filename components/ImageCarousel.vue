0b8af5dfbc128d87255b6abe0e6073219149332d
nickonometry <nickonometry@gmail.com>
2020-02-20 21:52:20 -0500

    started an image slider

--- components/ImageCarousel.vue
+++ components/ImageCarousel.vue
@@ -1,16 +1,66 @@
 
 
 <template>
-  <section class="carousel-container">Stuff here kiiiid</section>
+  <section>
+    <div class="carousel-container">
+      <v-card flat tile width="100%">
+        <v-window v-model="onboarding" reverse>
+          <v-window-item v-for="n in length" :key="`card-${n}`">
+            <v-card color="grey" height="320">
+              <v-row class="fill-height" align="center" justify="center" tag="v-card-text">
+                <h1 style="font-size: 5rem;" class="white--text">Slide {{ n }}</h1>
+              </v-row>
+            </v-card>
+          </v-window-item>
+        </v-window>
+
+        <v-card-actions class="justify-space-between">
+          <v-btn text @click="prev">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-item-group v-model="onboarding" class="text-center" mandatory>
+            <v-item v-for="n in length" :key="`btn-${n}`" v-slot:default="{ active, toggle }">
+              <v-btn :input-value="active" icon @click="toggle">
+                <v-icon>mdi-record</v-icon>
+              </v-btn>
+            </v-item>
+          </v-item-group>
+          <v-btn text @click="next">
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+    <v-divider></v-divider>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    length: 3,
+    onboarding: 0
+  }),
+
+  methods: {
+    next() {
+      this.onboarding =
+        this.onboarding + 1 === this.length ? 0 : this.onboarding + 1;
+    },
+    prev() {
+      this.onboarding =
+        this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .carousel-container {
+  width: 1024px;
   height: 456px;
-  width: 100vw;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
 }
 </style>
