482980a2c2ae2948f5ee7b9d676d4a6e83ffe25b
Adam Quinlan <aquinlan@tilsontech.com>
2020-02-21 14:52:29 -0500

    seperated out the enrollment section into comp

---
+++ components/EnrollmentStats.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="enrollment-rate">
+    <div class="enrollment-rate__grid">
+      <p class="callout">Nearpeer users are over 6 times more likely to enroll</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        adipisci facilis exercitationem praesentium sequi tempora, laboriosam
+        maxime unde quas aut soluta, placeat vel. Autem dolor minus
+        accusantium inventore neque. Obcaecati!
+      </p>
+      <img src="https://via.placeholder.com/400x400" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.enrollment-rate {
+  padding: 100px;
+
+  &__grid {
+    display: grid;
+    grid-template-columns: 2fr 4fr 4fr;
+    gap: 64px;
+
+    .callout {
+      font-size: 32px;
+      color: #4611a9;
+    }
+
+    img {
+      justify-self: center;
+      width: 100%;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,7 @@
     <HeroImage />
     <ImageCarousel />
 
-    <section class="enrollment-rate">
-      <div class="enrollment-rate__grid">
-        <p class="callout">Nearpeer users are over 6 times more likely to enroll</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-          adipisci facilis exercitationem praesentium sequi tempora, laboriosam
-          maxime unde quas aut soluta, placeat vel. Autem dolor minus
-          accusantium inventore neque. Obcaecati!
-        </p>
-        <img src="https://via.placeholder.com/400x400" />
-      </div>
-    </section>
+    <EnrollmentStats />
     <SocialMediaDiffers />
     <StrategicPartnership />
     <CallToAction link="/contact" buttonText="Contact Us">
@@ -30,6 +19,7 @@ import CallToAction from "../components/CallToAction";
 import ImageCarousel from "../components/ImageCarousel";
 import StrategicPartnership from "../components/StrategicPartnership";
 import SocialMediaDiffers from "../components/SocialMediaDiffers";
+import EnrollmentStats from "../components/EnrollmentStats";
 
 export default {
   components: {
@@ -37,29 +27,11 @@ export default {
     CallToAction,
     ImageCarousel,
     StrategicPartnership,
-    SocialMediaDiffers
+    SocialMediaDiffers,
+    EnrollmentStats
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.enrollment-rate {
-  padding: 100px;
-
-  &__grid {
-    display: grid;
-    grid-template-columns: 2fr 4fr 4fr;
-    gap: 64px;
-
-    .callout {
-      font-size: 32px;
-      color: #4611a9;
-    }
-
-    img {
-      justify-self: center;
-      width: 100%;
-    }
-  }
-}
 </style>
