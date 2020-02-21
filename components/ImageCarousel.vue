07de1d0b25228e9cd5eabcefdaafb4c4614a7f32
nickonometry <nickonometry@gmail.com>
2020-02-20 21:35:44 -0500

    starting image carousel

---
+++ components/ImageCarousel.vue
@@ -0,0 +1,16 @@
+
+
+<template>
+  <section class="carousel-container">Stuff here kiiiid</section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.carousel-container {
+  height: 456px;
+  width: 100vw;
+}
+</style>
