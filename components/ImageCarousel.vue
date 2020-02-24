c35171d562de0a43d7f7899a3746032061992425
nickonometry <nickonometry@gmail.com>
2020-02-23 21:02:24 -0500

    added slides

--- components/ImageCarousel.vue
+++ components/ImageCarousel.vue
@@ -8,11 +8,11 @@
       </v-btn>
       <v-card flat tile width="100%">
         <v-window v-model="onboarding" reverse>
-          <v-window-item v-for="n in length" :key="`card-${n}`">
+          <v-window-item>
             <div class="carousel-inner">
               <img src="/images/homepage-slider/lsu.png" />
               <div class="right">
-                <h3>Higher enrollment through peer engagement, powered by Nearpeer</h3>
+                <p>Higher enrollment through peer engagement, powered by Nearpeer</p>
                 <span>Including</span>
                 <ul>
                   <li>Out-of-state</li>
@@ -20,6 +20,79 @@
                   <li>Under-represented</li>
                   <li>While helping LSU have a small school feel</li>
                 </ul>
+              </div>
+            </div>
+          </v-window-item>
+          <v-window-item>
+            <div class="carousel-inner">
+              <img src="/images/homepage-slider/suu.png" />
+              <div class="right">
+                <p>“Nearpeer reduced our melt with out-of-state students, and other high-melt populations.</p>
+
+                <p>
+                  Our students using Nearpeer are much more likely to enroll. That’s why I want every incoming student to
+                  use it.”
+                </p>
+                <p class="quote-author">
+                  Brandon Wright AVP,
+                  <br />Enrollment Management Southern Utah University
+                </p>
+              </div>
+            </div>
+          </v-window-item>
+          <v-window-item>
+            <div class="carousel-inner">
+              <img src="/images/homepage-slider/umo.png" />
+              <div class="right">
+                <p>Higher enrollment & outcomes with Nearpeer</p>
+                <p>↑ yield ↓melt ↑ persistence</p>
+                <p>$1+ million lift in tuition revenue</p>
+              </div>
+            </div>
+          </v-window-item>
+          <v-window-item>
+            <div class="carousel-inner">
+              <img src="/images/homepage-slider/rnl.png" />
+              <div class="right">
+                <p>
+                  Ruffalo Noel Levitz & Nearpeer
+                  form strategic partnership to improve enrollment yield & reduce melt
+                </p>
+
+                <p>
+                  New for 2020, Nearpeer powers
+                  RNL’s melt prevention solution
+                </p>
+              </div>
+            </div>
+          </v-window-item>
+          <v-window-item>
+            <div class="carousel-inner">
+              <img src="/images/homepage-slider/contact.png" />
+              <div class="right">
+                <p>
+                  There’s still time to increase
+                  your 2020 class with Nearpeer!
+                </p>
+
+                <p>
+                  Easy to rollout, effective, guaranteed
+                  to reduce melt and improve yield
+                </p>
+
+                <p>Contact us today</p>
+              </div>
+            </div>
+          </v-window-item>
+          <v-window-item>
+            <div class="carousel-inner">
+              <img src="/images/homepage-slider/logo.png" />
+              <div class="right">
+                <p>Protect your incoming class today</p>
+
+                <p>Nearpeer accelerates incoming students’ connection & commitment to your institution</p>
+
+                <p>A strong defense against increased poaching from the 2020 NACAC changes</p>
               </div>
             </div>
           </v-window-item>
@@ -36,7 +109,7 @@
 <script>
 export default {
   data: () => ({
-    length: 3,
+    length: 6,
     onboarding: 0
   }),
 
@@ -66,15 +139,21 @@ export default {
     grid-template-columns: 450px auto;
     gap: 56px;
     align-items: center;
+    font-size: 18px;
 
     .right {
-      h3 {
-        margin: 0 0 16px 0;
+      p {
+        font-size: 18px;
+        line-height: 28px;
       }
 
       ul li {
         font-size: 16px;
         line-height: 32px;
+      }
+
+      .quote-author {
+        font-size: 16px;
       }
     }
   }
