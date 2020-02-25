eeea0dfc2fdebe7e57a8a99fd69815429179238d
Adam Quinlan <quinlanadam@gmail.com>
2020-02-25 10:09:26 -0500

    Copy Change

--- components/SocialMediaDiffers.vue
+++ components/SocialMediaDiffers.vue
@@ -9,7 +9,7 @@
         </div>
       </div>
       <p>
-        This students’ #1 priority in their decision to enroll.
+        This is students’ #1 priority in their decision to enroll.
         <br />Nearpeer addresses this need and delivers higher enrollment.
       </p>
     </div>
