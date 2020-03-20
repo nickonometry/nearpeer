52465b78c897d6858a80e208133f8246c1e76bb6
Adam Quinlan <aquinlan@tilsontech.com>
2020-03-19 22:31:35 -0400

    updated modal with actual code instead of png

--- components/CovidNotice.vue
+++ components/CovidNotice.vue
@@ -18,7 +18,42 @@
         </div>
       </template>
       <v-card height="auto">
-        <img class="covidletter" src="~assets/images/lettercovid.png" />
+        <div class="covidlettah">
+          <div class="letterheader">
+            <img src="/images/nearpeer_logo.svg" />
+            <div class="letright">
+              Nearpeer, Inc.
+              <br />63 Federal St.
+              <br />Portland, ME 04101
+            </div>
+          </div>
+          <h2>RE: ENROLLMENT CONTINGENCY PLANNING</h2>
+          <div class="letterbody">
+            <p>March 17, 2020</p>
+            <p>Dear Higher Education Leaders</p>
+            <p>Like you, the safety of students and staff is our highest concern. We also realize that uncertainty re: COVID-19 is challenging as it relates to your summer orientation and Fall 2020 class size. Nearpeer can help you get in front of that issue.</p>
+            <p>Nearpeer fills an important void in student-to-student engagement, especially if your events are online or streamed (e.g. Zoom, Facebook Live). The result is higher enrollment</p>
+            <p
+              class="quote"
+            >"With Nearpeer, our students are more likely to enroll. Especially in this unprecedented time of COVID-19, Nearpeer is critical to ensure our incoming students stay connected for the Fall."</p>
+            <p
+              class="quotefrom"
+            >Brandon Wright, AVP, Enrollment Management at Southern Utah University</p>
+            <p>Nearpeer is unlike anything you're doing today. Incoming students love using it to find, connect, and engage with classmates, regardless of location. This creates a sense of belonging, inclusion, and community. That's why students who use Nearpeer are 6X more likely to enroll - even if they haven't set foot on campus for orientation</p>
+            <p>We know results matter and, as a leader, you're likely under unprecedented pressure right now. That's why we guarantee results - and we make rollout fast and easy. Really.</p>
+            <p>
+              Interested in learning more?
+              <a href="/contact">Drop us a message here</a>. We'll follow up quickly - and respect your time. We'd look forward to exploring how we may serve you and your students for a successful 2020. Be safe.
+            </p>
+            <p>Best regards,</p>
+            <img class="dustinsig" src="~assets/images/dustinsig.svg" />
+            <p>
+              Dustin Manocha
+              <br />Founder &amp; CEO
+              <br />Nearpeer, Inc.
+            </p>
+          </div>
+        </div>
       </v-card>
     </v-dialog>
   </section>
@@ -36,6 +71,34 @@ export default {
 .covidletter {
   width: 100%;
   height: auto;
+}
+.covidlettah {
+  background-color: #fff;
+  padding: 20px 10px;
+  .letterheader {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    margin-bottom: 50px;
+    img {
+      width: 200px;
+      max-width: 50%;
+    }
+    .letright {
+      font-size: 13px;
+      color: #939393;
+    }
+  }
+  h2 {
+    text-align: center;
+    margin-bottom: 25px;
+  }
+  .letterbody {
+    padding: 0 5%;
+    .dustinsig {
+      width: 230px;
+    }
+  }
 }
 .covid {
   .covidcontainer {
